e6be99aedbd22ea20e4fe9cdb7cb770f12359ba2
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-17 12:14:09 -0300

    aula 04 - 06 Usando o try-with-resources

--- loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -9,24 +9,21 @@ public class TestaInsercaoComParametro {
 	public static void main(String[] args) throws SQLException {
 
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.getConnection();
-		connection.setAutoCommit(false);
-
-		PreparedStatement statement = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES(?, ?)",
-				Statement.RETURN_GENERATED_KEYS);
-
-		try {
-			adicionarVariavel("SmarTv", "45 polegadas", statement);
-			adicionarVariavel("Radio", "Radio de bateria", statement);
-			connection.commit();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTADO");
-			connection.rollback();
-		} finally {
+		try(Connection connection = connectionFactory.getConnection()){
+			connection.setAutoCommit(false);
+	
+			try (PreparedStatement statement = connection.prepareStatement(
+					"INSERT INTO PRODUTO (nome, descricao) VALUES(?, ?)", Statement.RETURN_GENERATED_KEYS);) {
+				adicionarVariavel("SmarTv", "45 polegadas", statement);
+				adicionarVariavel("Radio", "Radio de bateria", statement);
+				connection.commit();
+	
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
 			System.out.println("FECHANDO CONEXAO");
-			statement.close();
 			connection.close();
 		}
 	}
@@ -42,11 +39,12 @@ public class TestaInsercaoComParametro {
 
 		statement.execute();
 
-		ResultSet resultSet = statement.getGeneratedKeys();
-		while (resultSet.next()) {
-			Integer id = resultSet.getInt(1);
-			System.out.println("O id criado foi: " + id);
+		try (ResultSet resultSet = statement.getGeneratedKeys()) {
+			while (resultSet.next()) {
+				Integer id = resultSet.getInt(1);
+				System.out.println("O id criado foi: " + id);
+			}
 		}
-		resultSet.close();
+
 	}
 }
