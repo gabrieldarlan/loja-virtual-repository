05c1b4b1d3dd7898ce413a1d50365d26d7501f2f
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-17 12:59:33 -0300

    aula 05 - 05 Criando o pool de conexões

--- loja-virtual-repository/src/ConnectionFactory.java
+++ loja-virtual-repository/src/ConnectionFactory.java
@@ -1,12 +1,26 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
+	public DataSource dataSource;
+
+	// construtor criado para gerenciar pool de conexao
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl(ConstantesJDBC.URL);
+		comboPooledDataSource.setUser(ConstantesJDBC.USER);
+		comboPooledDataSource.setPassword(ConstantesJDBC.PASSWORD);
+	
+		this.dataSource = comboPooledDataSource;
+	}
+
+	// abre conexao com banco de dados
 	public Connection getConnection() throws SQLException {
-		Connection connection = DriverManager.getConnection(ConstantesJDBC.URL, ConstantesJDBC.USER,
-				ConstantesJDBC.PASSWORD);
-		return connection;
+		return this.dataSource.getConnection();
 	}
 }
